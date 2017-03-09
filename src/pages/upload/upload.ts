c0aff68aa1a0d0935ea2e68800f72ad8782a88c0
nikohol <nikohol@metropolia.fi>
2017-03-09 14:15:40 +0200

    mergeee

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -134,31 +134,6 @@ export class UploadPage {
     toast.onDidDismiss(() => {
       console.log('Dismissed toast');
     });
-
-<<<<<<< HEAD
-dataURItoBlob = (dataURI: any) => {
-    'use strict'
-    var byteString,
-      mimestring
-
-    if (dataURI.split(',')[0].indexOf('base64') !== -1) {
-      byteString = atob(dataURI.split(',')[1])
-    } else {
-      byteString = decodeURI(dataURI.split(',')[1])
-    }
-
-    mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-    var content = new Array();
-    for (var i = 0; i < byteString.length; i++) {
-      content[i] = byteString.charCodeAt(i)
-    }
-
-    return new Blob([new Uint8Array(content)], { type: mimestring });
-  }
-
-
-=======
     toast.present();
     }
     
@@ -206,5 +181,24 @@ dataURItoBlob = (dataURI: any) => {
       console.log(err);
     });    
   }
->>>>>>> nikohol
+  dataURItoBlob = (dataURI: any) => {
+    'use strict'
+    var byteString,
+      mimestring
+
+    if (dataURI.split(',')[0].indexOf('base64') !== -1) {
+      byteString = atob(dataURI.split(',')[1])
+    } else {
+      byteString = decodeURI(dataURI.split(',')[1])
+    }
+
+    mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    var content = new Array();
+    for (var i = 0; i < byteString.length; i++) {
+      content[i] = byteString.charCodeAt(i)
+    }
+
+    return new Blob([new Uint8Array(content)], { type: mimestring });
+  }
 }
