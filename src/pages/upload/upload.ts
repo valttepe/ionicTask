031ce743ea22cb07e7b8735ed246b1fcc3b48802
valttepe <Valtteri.Pesu@metropolia.fi>
2017-03-09 13:43:57 +0200

    UrlToBlob

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -126,4 +126,26 @@ export class UploadPage {
   toast.present();
 }
 
+dataURItoBlob = (dataURI: any) => {
+    'use strict'
+    var byteString,
+      mimestring
+
+    if (dataURI.split(',')[0].indexOf('base64') !== -1) {
+      byteString = atob(dataURI.split(',')[1])
+    } else {
+      byteString = decodeURI(dataURI.split(',')[1])
+    }
+
+    mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    var content = new Array();
+    for (var i = 0; i < byteString.length; i++) {
+      content[i] = byteString.charCodeAt(i)
+    }
+
+    return new Blob([new Uint8Array(content)], { type: mimestring });
+  }
+
+
 }
